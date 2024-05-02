f653e1bd75714891af819136ca92113b750db661
bcaro57 <bencaro@udel.edu>
2024-05-02 19:51:20 -0400

    added some comments and the 'pindefs' header, which just helps clean everything up

--- Mechatronics Robot/src/main.cpp
+++ Mechatronics Robot/src/main.cpp
@@ -2,17 +2,22 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <Adafruit_PWMServoDriver.h>
+
 #include "pindefs.h"
 
 
-int max_speed = 1720;
-int min_speed = 1280;
+// These variables were determined based on some testing of the motors
 int L_Lower_limit_stop=1438;
 int L_Upper_limit_stop=1487;
 int R_Lower_limit_stop=1434;
 int R_Upper_limit_stop=1482;
+
+// This gives us our "stopped speed", which allows us to 
 int L_stopped_speed = (L_Upper_limit_stop+L_Lower_limit_stop)/2;
 int R_stopped_speed = (R_Upper_limit_stop+R_Lower_limit_stop)/2;
+
+// Miscellanious variables
+int speed = 120;
 int run_speed = 75; // if 20 or below it will be stopped, and should exceeding 220 has no effec
 long timer = 0;
 long t = 0;
@@ -21,27 +26,34 @@ long led_blink_time = 0;
 int count = 0;
 bool counting = false;
 bool has_seen = false;
-int Fire_detector=6;
 
+// Servo initialization
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
 
+// Radio initialization
 RF24 radio(7, 8); // CE, CSN
-
 const byte address[6] = "00001";
 
-// Max size of this struct is 32 bytes - NRF24L01 buffer limit
+/* 
+This is the package of data from our controller. It includes 'Lefty' and 'Righty', which are values
+that control the speed of our two wheels given to us by the controller. It also has 'Single', 'Double', 
+and 'border', which are updated by the robot and seen on our controller.
+
+The max size of this struct is 32 bytes - that is the NRF24L01 buffer limit.
+*/
 struct Data_Package {
-  int Lefty=90;
-  int Righty = 1024;
+  int Lefty = 0;
+  int Righty = 0;
   int Single = 0;
   int Double = 0;
-  int border=0;
+  int border = 0;
 };
 
-Data_Package data; //Create a variable with the above structure
+// Create a variable with the above structure
+Data_Package data;
 
-
+// Initializing our functions 
 void Receive_data();
 void Move_Motors(int Left_speed,int Right_speed);
 void Detect_Fire();
@@ -49,12 +61,15 @@ void counting_lines();
 
 
 void setup() {
+  // Pin initialization
   pinMode(ledPinRed, OUTPUT);    // initialize the red LED pin as an output
   pinMode(ledPinGreen, OUTPUT);  // initialize the green LED pin as an output
   pinMode(irSensorPin0, INPUT);  // initialize the IR sensor pin as an input
   pinMode(Fire_detector, INPUT);  // initialize the IR sensor pin as an input
-Serial.begin(9600);
-    if (!radio.begin()) {
+
+  // Radio setup
+  Serial.begin(9600);
+  if (!radio.begin()) {
     Serial.println(F("radio hardware is not responding!!"));
     while (1) {}  // hold in infinite loop
   }
@@ -62,16 +77,16 @@ Serial.begin(9600);
   radio.setPayloadSize(sizeof(Data_Package)); 
   radio.setPALevel(RF24_PA_MIN);
 
-  //Added Stuff
-  
+  // Servo setup
   pwm.begin();
   pwm.setOscillatorFrequency(27000000);
   pwm.setPWMFreq(SERVO_FREQ);  
   delay(10);
-
 }
 
 void loop() {
+  // calibration sequence - only used prior to actual testing
+
   // pwm.writeMicroseconds(9, 1444);
   // for (int i=1420;i<1490;i++){
   //   pwm.writeMicroseconds(10, i);
@@ -90,32 +105,41 @@ void Receive_data(){
   radio.startListening();
   if (radio.available()) {
     while (radio.available()) {
-  radio.read(&data, sizeof(Data_Package)); // Read the whole data and store it into the 'data' structure
-  // Serial.print("Lefty: ");
-  // Serial.println(data.Righty);
-    }}
+    radio.read(&data, sizeof(Data_Package)); // Read the whole data and store it into the 'data' structure
+    // Serial.print("Lefty: ");
+    // Serial.println(data.Righty);
+    }
+  }
 }
+
+/*
+This function is used for us to manually control the speeds of our two wheels, labeled as left and right.
+*/ 
 void Move_Motors(int Left_speed,int Right_speed){
   if (Left_speed>700){
-    pwm.writeMicroseconds(9, L_stopped_speed+100);
+    pwm.writeMicroseconds(9, L_stopped_speed + speed);
   }
   else if (Left_speed<200){
-    pwm.writeMicroseconds(9, L_stopped_speed-100);
+    pwm.writeMicroseconds(9, L_stopped_speed - speed);
   }
   else{
     pwm.writeMicroseconds(9, L_stopped_speed);
   }
   
-    if (Right_speed>700){
-    pwm.writeMicroseconds(10, R_stopped_speed-100);
+  if (Right_speed>700){
+    pwm.writeMicroseconds(10, R_stopped_speed - speed);
   }
    else if (Right_speed<200){
-    pwm.writeMicroseconds(10,R_stopped_speed+100);
+    pwm.writeMicroseconds(10,R_stopped_speed + speed);
    }
   else{
     pwm.writeMicroseconds(10, R_stopped_speed);
   }
 }
+
+/*
+This function is used for us to detect the IR signal from the 'fire', and respond accordingly by moving our 'ladder'.
+*/ 
 void Detect_Fire(){
   int Fire_State=digitalRead(Fire_detector);
   if(Fire_State==0){
@@ -130,6 +154,9 @@ void Detect_Fire(){
   // Serial.println((millis()-t_4_fire));
 }
 
+/*
+This function is used for us to count the lines and determine whether they are a double or a single line.
+*/ 
 void counting_lines(){
  t = millis();
   if (digitalRead(irSensorPin0) == LOW && !has_seen) {
@@ -143,12 +170,10 @@ void counting_lines(){
     delay(50);
   }
 
-
   if (t - timer > 750){
     counting = false;
   }
   //Serial.println(count);
-
 
   // // after leaving the counting state, we write our LED's according to the count, and allow them to shine for half a second. after that, they are all turned off (when the count returns to 0).
   if (count == 1 && !counting) {
@@ -171,5 +196,4 @@ void counting_lines(){
     digitalWrite(ledPinRed, LOW);
     digitalWrite(ledPinGreen, LOW);
   }
-
  }
--- Mechatronics Robot/src/pindefs.h
+++ Mechatronics Robot/src/pindefs.h
@@ -1,5 +1,6 @@
-const int ledPinRed = 11;      // the number of the red LED pin
-const int ledPinGreen = 12;    // the number of the green LED pin
-const int irSensorPin0 = 36;  // the number of the IR sensor pin
-const int right_sensor = 38;
-const int left_sensor = 34;
+int ledPinRed = 11;      // the number of the red LED pin
+int ledPinGreen = 12;    // the number of the green LED pin
+int irSensorPin0 = 36;  // the number of the IR sensor pin
+int right_sensor = 38;
+int left_sensor = 34;
+int Fire_detector=6;
