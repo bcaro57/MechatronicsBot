c71b6ae17c7a5781e9b1728699e4415f69fd4cd2
camrynscully <cbscully@udel.edu>
2024-05-09 02:32:52 -0400

    Updated the platform to switch to the earle philhower core. Minor changes to main.cpp

--- Mechatronics Pico W/src/main.cpp
+++ Mechatronics Pico W/src/main.cpp
@@ -1,19 +1,33 @@
 #include <Arduino.h>
 
-int led = PIN_LED;
+int external_led = 15;
 
 void setup() {
-  pinMode(led, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(external_led, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(led, HIGH);
-  Serial.println("LED ON");
+
+  // Basic LED Blink - To test configuration
+  // For some reason the on board LED does not blink (thought maybe it was the pico w
+  // because the LED deals with a few other things), but I got it to blink using python initially
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(external_led, HIGH);
+  Serial.println("1");
   delay(500);
 
-  digitalWrite(led, LOW);
-  Serial.println("LED OFF");
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(external_led, LOW);
+  Serial.println("0");
   delay(500);
+
+  // if (Serial.available() > 0) {
+  //   char incomingByte = Serial.read();
+
+  //   Serial.print("Received: ");
+  //   Serial.println(incomingByte);
+  // }
+
 }
-
